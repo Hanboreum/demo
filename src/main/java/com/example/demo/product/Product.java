824b2c46236038eb7975b844943f682e3f0b170c
fullm <matoma1_@naver.com>
2024-04-21 01:35:04 +0900

    @Id 식별자 추가

--- src/main/java/com/example/demo/product/Product.java
+++ src/main/java/com/example/demo/product/Product.java
@@ -1,10 +1,12 @@
 package com.example.demo.product;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 @Entity // entity에 등록 관리
 @Data
 public class Product {
+    @Id
     private int id;
     private String name;
     private int price;
