e8a8747bb1768b84e6054b15e78888610e3ec982
fullm <matoma1_@naver.com>
2024-04-21 01:03:34 +0900

    id 추가

--- src/main/java/com/example/demo/product/Product.java
+++ src/main/java/com/example/demo/product/Product.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Entity // entity에 등록 관리
 @Data
 public class Product {
-    //private int id;
+    private int id;
     private String name;
     private int price;
     private String description;
